fcaf860258cc588dc2d3f9f893d07fc7ab30f149
elan <elavarasan.n198@gmail.com>
2025-10-07 17:26:12 +0530

    var

--- Infra/aws/providers.tf
+++ Infra/aws/providers.tf
@@ -3,7 +3,6 @@ terraform {
     # The bucket must be created manually before running terraform init -reconfigure
     bucket         = "tf-state-nodejs-crud-${var.project_name}"
     key            = "state/${var.project_name}.tfstate"
-    region         = var.aws_region
     encrypt        = true
     dynamodb_table = "terraform-locks" # Must be created manually with Partition Key 'LockID' (String)
   }
